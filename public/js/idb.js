3a309fcfa4898681f5524a58cdbb20fe3d054d94
JCH95 <jhorger95@gmail.com>
2022-05-16 18:42:46 -0500

    added save record function idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,7 +18,19 @@ request.onsuccess = function(event) {
   
     // check if app is online
     if (navigator.onLine) {
-        // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadPizza();
+        uploadBudget();
     }
 };
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new budget and there's no internet connection
+function saveRecord(record) { 
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+  
+    pizzaObjectStore.add(record);
+}
