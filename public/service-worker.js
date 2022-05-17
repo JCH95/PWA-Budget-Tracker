254f06bbf357bb64189ab62c709ab5c6bfb44969
JCH95 <jhorger95@gmail.com>
2022-05-16 22:50:30 -0500

    added install event listener to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,4 +8,11 @@ const FILES_TO_CACHE = [
     "./js/index.js"
 ];
 
-  
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
